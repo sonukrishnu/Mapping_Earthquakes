632658607a6874d13dc9a79602e2319e4587e427
unknown <swati789@gmail.com>
2022-04-01 15:47:23 -0700

    Uploading work

--- Earthquake_Challenge/static/js/Challenge_logic.js
+++ Earthquake_Challenge/static/js/Challenge_logic.js
@@ -23,6 +23,17 @@ let satelliteStreets = L.tileLayer(
   }
 );
 
+// We create the second tile layer that will be the background of our map.
+let Dark = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+  }
+);
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map("mapid", {
   center: [40.7, -94.5],
@@ -34,6 +45,7 @@ let map = L.map("mapid", {
 let baseMaps = {
   Streets: streets,
   Satellite: satelliteStreets,
+  Dark: Dark
 };
 
 // 1. Add a 2nd layer group for the tectonic plate data.
